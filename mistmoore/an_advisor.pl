efd58a81f6772c117d586bc89ce9e2d1fea52559
huffin <acg1089@aol.com>
2019-12-03 19:55:35 -0600

    Update an_advisor.pl
    flavor flav

--- mistmoore/an_advisor.pl
+++ mistmoore/an_advisor.pl
@@ -1,5 +1,6 @@
 sub EVENT_SAY {
 	if($text=~/hail/i) {
 		quest::attack($name);
+		quest::say("How dare you speak to me, you impudent sack of nauseating refuse! For this outrage of insubordination I call forth the vengeance of House Mistmoore! You now have little time left to live.  Enjoy it.");
 	}
 }
